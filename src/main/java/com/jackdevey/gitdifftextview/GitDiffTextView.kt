12076bc0039a1399c6b3d304bc100c961cb1b97e
Jack Devey <jack@bandev.uk>
2021-01-24 00:47:10 +0000

    Update GitDiffTextView.kt

--- src/main/java/com/jackdevey/gitdifftextview/GitDiffTextView.kt
+++ src/main/java/com/jackdevey/gitdifftextview/GitDiffTextView.kt
@@ -1,4 +1,4 @@
-package com.jackdevey.gitdifftextview
+package com.alorma.diff.lib
 
 import android.annotation.TargetApi
 import android.content.Context
@@ -11,51 +11,49 @@ import android.text.TextUtils
 import android.text.style.BackgroundColorSpan
 import android.util.AttributeSet
 import android.widget.TextView
+import com.jackdevey.gitdifftextview.R
 
 /**
  * Created by Bernat on 22/12/2014.
- * Converted to kotlin & adapted for dark mode by Jack Devey 24/01/2021.
  */
 class GitDiffTextView : TextView {
     private var additionColor = 0
     private var deletionColor = 0
-    private var showInfo = false
     private var maxLines = -1
 
-    @JvmOverloads
-    constructor(context: Context, attrs: AttributeSet? = null) : super(context, attrs) {
-        init(context, attrs, R.attr.diff_theme)
+    constructor(context: Context?) : super(context) {
+        init()
     }
 
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
-        init(context, attrs, defStyleAttr)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init()
     }
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes) {
-        init(context, attrs, defStyleAttr)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes) {
+        init()
     }
 
-    private fun init(context: Context, attrs: AttributeSet?, defStyleAttr: Int) {
+    private fun init() {
         isInEditMode
-        val array = context.obtainStyledAttributes(attrs,
-                R.styleable.DiffTextViewStyle, defStyleAttr, 0)
         when (context.resources?.configuration?.uiMode?.and(Configuration.UI_MODE_NIGHT_MASK)) {
             Configuration.UI_MODE_NIGHT_YES -> {
-                additionColor = array.getColor(R.styleable.DiffTextViewStyle_diff_addition_color, Color.parseColor("#2f9e2f"))
-                deletionColor = array.getColor(R.styleable.DiffTextViewStyle_diff_deletion_color, Color.parseColor("#cf2929"))
+                additionColor = Color.parseColor("#2f9e2f")
+                deletionColor = Color.parseColor("#cf2929")
             }
             Configuration.UI_MODE_NIGHT_NO -> {
-                additionColor = array.getColor(R.styleable.DiffTextViewStyle_diff_addition_color, Color.parseColor("#CCFFCC"))
-                deletionColor = array.getColor(R.styleable.DiffTextViewStyle_diff_deletion_color, Color.parseColor("#FFDDDD"))
+                additionColor = Color.parseColor("#CCFFCC")
+                deletionColor = Color.parseColor("#FFDDDD")
             }
         }
-        showInfo = array.getBoolean(R.styleable.DiffTextViewStyle_diff_show_diff_info, false)
-        array.recycle()
     }
 
     override fun setText(text: CharSequence, type: BufferType) {
-        if (!TextUtils.isEmpty(text)) {
+        if (!TextUtils.isEmpty(text) && type == BufferType.NORMAL) {
             val diff = text.toString()
             val split = diff.split("\\r?\\n|\\r").toTypedArray()
             if (split.size > 0) {
@@ -68,13 +66,15 @@ class GitDiffTextView : TextView {
                 }
                 for (i in 0 until lines) {
                     var token = split[i]
-                    if (!token.startsWith("@@") || showInfo) {
+                    if (!token.startsWith("@@")) {
                         if (i < lines - 1) {
                             token = """
                                 $token
                                 
                                 """.trimIndent()
                         }
+                        var span: BackgroundColorSpan? = null
+                        var spannableDiff = SpannableString(token)
                         val firstChar = token[0]
                         var color = 0
                         if (firstChar == '+') {
@@ -82,9 +82,9 @@ class GitDiffTextView : TextView {
                         } else if (firstChar == '-') {
                             color = deletionColor
                         }
-                        val spannableDiff = SpannableString(token)
+                        spannableDiff = SpannableString(token)
                         if (color == additionColor || color == deletionColor) {
-                            val span = BackgroundColorSpan(color)
+                            span = BackgroundColorSpan(color)
                             spannableDiff.setSpan(span, 0, token.length, SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE)
                         }
                         builder.append(spannableDiff)
